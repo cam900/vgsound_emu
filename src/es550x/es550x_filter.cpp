9510c700a12e79c4b7b42913d6fdbe8e50ea0ee1
cam900 <dbtlrchl@naver.com>
2022-09-07 15:52:18 +0900

    Fix compile

--- src/es550x/es550x_filter.cpp
+++ src/es550x/es550x_filter.cpp
@@ -16,7 +16,13 @@ void es550x_shared_core::es550x_voice_t::es550x_filter_t::reset()
 	m_lp = 0;
 	m_k2 = 0;
 	m_k1 = 0;
-	std::fill(m_o.begin(), m_o.end(), 0);
+	for (int p = 0; p < 5; p++)
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			m_o[p][i] = 0;
+		}
+	}
 }
 
 void es550x_shared_core::es550x_voice_t::es550x_filter_t::tick(s32 in)
