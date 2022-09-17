124e00afedab79e31fb0c29a30913d2ac60c2192
cam900 <dbtlrchl@naver.com>
2022-09-17 12:53:57 +0900

    Fix compile

--- src/core/vox/vox.hpp
+++ src/core/vox/vox.hpp
@@ -39,14 +39,14 @@ class vox_core : public vgsound_emu_core
 
 						s32 step() { return m_step; }
 
-						decoder_state_t &operator=(decoder_state_t src)
+						decoder_state_t &operator=(decoder_state_t &src)
 						{
 							copy_state(src);
 							return *this;
 						}
 
 					private:
-						void copy_state(decoder_state_t src);
+						void copy_state(decoder_state_t &src);
 
 						vox_core &m_vox;
 						s8 m_index = 0;
